b3f8cb6e7006af781d6c16dde5e3c6eb455baf9f
Dominique Garmier <dominique@garmier.ch>
2022-05-12 18:37:33 +0200

    0

---
+++ schema_manager/commands/build.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import configparser
+from argparse import Namespace
+
+
+def build(args: Namespace) -> int:
+    '''makes schema installable'''
+
+    config = configparser.ConfigParser()
+    config['']
+
+    return 0
